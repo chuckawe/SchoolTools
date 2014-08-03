0fcb157ecddc5e2c575260566432e3f3f4826194
chuckawe <chuckawe@gmail.com>
2014-08-03 14:59:43 -0400

    Fixe id/day; behave/id

--- detent.py
+++ detent.py
@@ -36,14 +36,18 @@ for row in culture:
 
 print dates
 <<<<<<< HEAD
+<<<<<<< HEAD
 
 =======
 >>>>>>> master
+=======
+>>>>>>> 05c3acc949cd199d739307e4e510f59cbbca32b4
 
 # Placeholder for reading in of lateness data
 # 
 
 
+<<<<<<< HEAD
 # Discipline Policy enforced in this part of code
 # Loop over dates
 for date in dates.keys():
@@ -69,13 +73,35 @@ for date in dates.keys():
     # Loop over student and group them into behaviors
 
 <<<<<<< HEAD
+=======
+reader.close()
+
+>>>>>>> 05c3acc949cd199d739307e4e510f59cbbca32b4
 # Discipline Policy enforced in this part of code
-rownum = 0
 # Loop over dates
 for date in dates.keys():
+<<<<<<< HEAD
     # Loop over students for that day
     if stu_id in dates.[date]:
     	
+=======
+    # Total students
+    print date, len(dates[date].keys())
+
+print 'hello'
+
+# Loop over dates and returns total for each student
+for date in dates.keys():
+    for stu_id in dates[date].keys():
+        nDemerits= dates[date][stu_id][0]
+        nAutoDTs= dates[date][stu_id][1]
+        nSendOuts= dates[date][stu_id][2]
+    print date, stu_id, nDemerits, nAutoDTs, nSendOuts
+
+[dates[date][stu_id][x] for x in dates[date].keys()]
+
+    # Loop over student and group them into behaviors
+>>>>>>> 05c3acc949cd199d739307e4e510f59cbbca32b4
     pass
 =======
 >>>>>>> master
