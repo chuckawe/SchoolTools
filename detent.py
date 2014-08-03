d3de163cd631e9aaf8f486917fc20959fc0447ba
chuckawe <chuckawe@gmail.com>
2014-08-03 14:26:02 -0400

    Fixed id/day; behave/id

--- detent.py
+++ detent.py
@@ -6,10 +6,7 @@ culture = csv.reader(reader, delimiter=',')
         	
 # date dictionary
 dates = {}
-# listing of behaviors committed
-#behaviors =[]	
-# listing of IDs
-#whodunit =[]
+
 n = 0
 for row in culture:
     if n>100: break
@@ -19,11 +16,11 @@ for row in culture:
     # Check if date is already stored
     if date not in dates.keys():
 	    dates[date] = {}
-    
+
     # Check if Student is already stored
     if stu_id not in dates[date].keys():
         dates[date][stu_id] = [0, 0, 0, False, True] # [ nDemerits, nAutoDTs, nSendOuts, If Late, If Late Egreg   ]
-	
+
     if row[5] == 'Demeritable Behaviors':
         # This means we have a demerit
         dates[date][stu_id][0] += 1
@@ -33,24 +30,45 @@ for row in culture:
     elif row [4] == "Sent Out":
     	# This means we have a Send Out
     	dates[date][stu_id][2] +=1
-    
-    
-        
+# elif
     
     n += 1
 
 print dates
+<<<<<<< HEAD
 
+=======
+>>>>>>> master
 
 # Placeholder for reading in of lateness data
 # 
 
 
+# Discipline Policy enforced in this part of code
+# Loop over dates
+for date in dates.keys():
+    # Total students
+    print date, len(dates[date].keys())
 
-reader.close()
+print 'hello'
+n=0
+# Loop over dates and returns total for each student
+for date in dates.keys():
+    for stu_id in dates[date].keys():
+            nDemerits= dates[date][stu_id][0]
+            nAutoDTs= dates[date][stu_id][1]
+            nSendOuts= dates[date][stu_id][2]
+            print date, stu_id, nDemerits, nAutoDTs, nSendOuts
 
+# for stu_id in dates[date].keys():
+    x = sum(dates[date][stu_id][0])
+    print x
 
+# [dates[date][stu_id][x] for x in dates[date].keys()]
 
+    # Loop over student and group them into behaviors
+
+<<<<<<< HEAD
 # Discipline Policy enforced in this part of code
 rownum = 0
 # Loop over dates
@@ -59,8 +77,10 @@ for date in dates.keys():
     if stu_id in dates.[date]:
     	
     pass
+=======
+>>>>>>> master
 
-# Used to save header info
+# Used to save header info for writing new file
 #    if rownum == 0:
 #        header = row
 #    else:
