459c5651f34a7345c33c8197a2eb4b533491398d
Charles Awe <chuckawe@gmail.com>
2014-08-05 13:40:28 -0400

    Total 3+ & Sent out

--- detent.py
+++ detent.py
@@ -9,7 +9,7 @@ dates = {}
 
 n = 0
 for row in culture:
-    if n>300: break
+    if n>500: break
     date = row[8]
     stu_id = row[1]
     
@@ -27,10 +27,9 @@ for row in culture:
     elif row[5] == "Auto-Detention":
         # This means we have a Auto DT assigned 
         dates[date][stu_id][1] += 1
-    elif row [4] == "Sent Out":
+    elif row [4] == "Sent out":
     	# This means we have a Send Out
     	dates[date][stu_id][2] +=1
-# elif
     
     n += 1
 
@@ -47,34 +46,39 @@ for date in dates.keys():
     print date, len(dates[date].keys())
 
 print 'hello'
-n=0
 
 
 # Loop over dates and returns total for each student
 for date in dates.keys():
+    nthdemPerDay = 0
+    nthsentPerDay =0
     for stu_id in dates[date].keys():
             nDemerits= dates[date][stu_id][0]
             nAutoDTs= dates[date][stu_id][1]
             nSendOuts= dates[date][stu_id][2]
             print date, stu_id, nDemerits, nAutoDTs, nSendOuts
+            if nDemerits >=3:
+                nthdemPerDay +=1
+            if nSendOuts >=2:
+                nthsentPerDay +=1
+    print 'For day,', date ,'there were' ,nthdemPerDay ,'students with 3+ Dem'
+    print 'For day,', date ,'there were' ,nthsentPerDay ,'students sent out more than once'
 
-for date in dates.keys():
-    for stu_id in dates[date].keys():
-        for behave in dates[date][stu_id]:
-            if [0]>= 3:
-                print 'Hello'
-            else:
-                print 'no'
+
+
+#for date in dates.keys():
+#   for stu_id in dates[date].keys():
+#       for behave in dates[date][stu_id]:
+
 
 
 # for stu_id in dates[date].keys():
-    x = sum(dates[date][stu_id][0])
-    print x
+#x = sum(dates[date][stu_id][0])
+#print x
 
 # [dates[date][stu_id][x] for x in dates[date].keys()]
 
     # Loop over student and group them into behaviors
-
 
 # Used to save header info for writing new file
 #    if rownum == 0:
@@ -84,7 +88,4 @@ for date in dates.keys():
 #        for col in row:
 #            print '%-8s: %s' % (header[colnum], col)
 #            colnum += 1
-                
 #    rownum +=1
-    
-	
