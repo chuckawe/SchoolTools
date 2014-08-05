3b617b2f30d765bcb8c544c31cb79c4022fd578a
Charles Awe <chuckawe@gmail.com>
2014-08-05 12:05:40 -0400

    Update

--- detent.py
+++ detent.py
@@ -9,7 +9,7 @@ dates = {}
 
 n = 0
 for row in culture:
-    if n>100: break
+    if n>300: break
     date = row[8]
     stu_id = row[1]
     
@@ -35,36 +35,16 @@ for row in culture:
     n += 1
 
 print dates
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
 
-=======
->>>>>>> master
-=======
->>>>>>> 05c3acc949cd199d739307e4e510f59cbbca32b4
-=======
->>>>>>> master
 
 # Placeholder for reading in of lateness data
-# 
+#
 
-
-<<<<<<< HEAD
 # Discipline Policy enforced in this part of code
 # Loop over dates
 for date in dates.keys():
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> master
-# Discipline Policy enforced in this part of code
-# Loop over dates
-for date in dates.keys():
-    # Total students
+    #Total stud/day
     print date, len(dates[date].keys())
-<<<<<<< HEAD
 
 print 'hello'
 n=0
@@ -76,56 +56,7 @@ for date in dates.keys():
             nSendOuts= dates[date][stu_id][2]
             print date, stu_id, nDemerits, nAutoDTs, nSendOuts
 
-=======
 
-print 'hello'
-n=0
-# Loop over dates and returns total for each student
-for date in dates.keys():
-    for stu_id in dates[date].keys():
-            nDemerits= dates[date][stu_id][0]
-            nAutoDTs= dates[date][stu_id][1]
-            nSendOuts= dates[date][stu_id][2]
-            print date, stu_id, nDemerits, nAutoDTs, nSendOuts
-
->>>>>>> master
-# for stu_id in dates[date].keys():
-    x = sum(dates[date][stu_id][0])
-    print x
-
-# [dates[date][stu_id][x] for x in dates[date].keys()]
-
-    # Loop over student and group them into behaviors
-<<<<<<< HEAD
-
-<<<<<<< HEAD
-=======
-reader.close()
-
->>>>>>> 05c3acc949cd199d739307e4e510f59cbbca32b4
-# Discipline Policy enforced in this part of code
-# Loop over dates
-for date in dates.keys():
-<<<<<<< HEAD
-    # Loop over students for that day
-    if stu_id in dates.[date]:
-    	
-=======
->>>>>>> origin/from-school
-    # Total students
-    print date, len(dates[date].keys())
-
-print 'hello'
-
-# Loop over dates and returns total for each student
-for date in dates.keys():
-    for stu_id in dates[date].keys():
-        nDemerits= dates[date][stu_id][0]
-        nAutoDTs= dates[date][stu_id][1]
-        nSendOuts= dates[date][stu_id][2]
-<<<<<<< HEAD
-
-        print date, stu_id, nDemerits, nAutoDTs, nSendOuts
 
 # for stu_id in dates[date].keys():
     x = sum(dates[date][stu_id][0])
@@ -135,20 +66,6 @@ for date in dates.keys():
 
     # Loop over student and group them into behaviors
 
-=======
-    print date, stu_id, nDemerits, nAutoDTs, nSendOuts
-
-[dates[date][stu_id][x] for x in dates[date].keys()]
-
-    # Loop over student and group them into behaviors
->>>>>>> 05c3acc949cd199d739307e4e510f59cbbca32b4
-    pass
-=======
->>>>>>> master
-=======
-
->>>>>>> master
->>>>>>> origin/from-school
 
 # Used to save header info for writing new file
 #    if rownum == 0:
